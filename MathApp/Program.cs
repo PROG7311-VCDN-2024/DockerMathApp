b1361d9895d81b29afc55efa03d1fb3aa06f85f1
lab_services_student <lab_services_student@labVMH8OX>
2024-04-14 21:01:35 +0200

    Add project files.

---
+++ MathApp/Program.cs
@@ -0,0 +1,43 @@
+using MathApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace MathApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<MathDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Math_DB")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Math}/{action=Calculate}/{id?}");
+
+            app.Run();
+        }
+    }
+}
